f25b06fb55c796329757dad9dca8b59cb0cdfc84
Stig Sandø <sirfufie@gmail.com>
2018-12-09 19:45:57 +0100

    Allow processing to be sequential when target-tile is not explicit

--- ProcessTile/process.py
+++ ProcessTile/process.py
@@ -9,11 +9,11 @@ from PIL import Image
 class TileTransfer:
     filename = None
     source_tile = 0
-    target_tile = 0
+    target_tile = -1
     source_xoffset = 0
     source_yoffset = 0
     
-    def __init__(self, filename, source_tile, target_tile, source_xoffset = 0, source_yoffset = 0):
+    def __init__(self, filename, source_tile, target_tile = -1, source_xoffset = 0, source_yoffset = 0):
         self.filename = filename
         self.source_tile = source_tile
         self.target_tile = target_tile
@@ -40,7 +40,10 @@ def tilenumber_to_box(image_width, image_height, tile_number, xoffset = 0, yoffs
     return (tile_column * 32 + xoffset, tile_row * 32 + yoffset, tile_column * 32 + 32 + xoffset, tile_row * 32 + 32 + yoffset)
 
 def as_transfer(entry):
-    return TileTransfer(entry['filename'], entry['source_tile'], entry['target_tile'])
+    return TileTransfer(entry['filename'], source_tile = entry['source-tile'],
+                        target_tile = entry['target-tile'] if 'target-tile' in entry else -1,
+                        source_xoffset = entry['source-x-offset'] if 'source-x-offset' in entry else 0,
+                        source_yoffset = entry['source-y-offset'] if 'source-y-offset' in entry else 0)
 
 def as_tilefilespec(entry, verbose = False):
     return TileFileSpecification(entry['output'], entry['width'], entry['height'], [as_transfer(x) for x in entry['tiles']])
@@ -53,13 +56,18 @@ def parse_tilefilespec(tilefile, verbose = False):
 def process_tilespec(entry, outfile, verbose = False):
     output = Image.new('RGBA', (entry.width, entry.height), (255,0,0,255))
 
+    last_index = -1
     for input_spec in entry.tiles:
         with Image.open(input_spec.filename) as input:
             original = tilenumber_to_box(input.width, input.height, input_spec.source_tile,
                                          xoffset=input_spec.source_xoffset,
                                          yoffset=input_spec.source_yoffset)
             region = input.crop(original)
-            target = tilenumber_to_box(output.width, output.height, input_spec.target_tile)
+            to_target = input_spec.target_tile
+            if to_target < 0:
+                to_target = last_index + 1
+            target = tilenumber_to_box(output.width, output.height, tile_number = to_target)
+            last_index = to_target
             output.paste(region, target)
             
     if verbose:
